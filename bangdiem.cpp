19fca70f9f8ca0925d12da5463c5fb7348ce9803
Nguyễn hữu đức <47705754+DucNguyenTHT@users.noreply.github.com>
2019-02-22 20:20:25 +0700

    Create bangdiem.cpp

---
+++ bangdiem.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<fstream>
+#include<string.h>
+using namespace std;
+struct bangDiem
+{
+	char tenHocSinh[100];
+	float diemMonToan;
+	float diemMonLy;
+	float diemMonHoa;
+	float diemMonTin;
+	float diemMonAnh;
+};
+bangDiem hocSinh[100];
+int N=0;//hocsinh thu N
+
+#include"bangdiem.h"
+int main(){
+	docFile();
+	int chon;
+	do 
+	{
+		chon=suLuaChon();
+		switch(chon)
+		{
+		case 1: cin.ignore();themHocSinhVaDiem();break;
+		case 2: hienThi();break;
+		case 3: xoaHocSinhVaDiem();break;
+		case 4: fixTenVaDiem();break;
+		case 0: ghiFile();break;
+		}
+		system("pause");
+		system("cls");
+	}while(chon!=0);
+	return 0;
+	}
